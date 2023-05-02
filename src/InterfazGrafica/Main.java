6bc1d7bcd65ed7ef7a371c2490a79e1478474df3
migue02ax <axel_fuster_barba@hotmail.com>
2023-05-02 14:53:37 -0600

    mandando tarea

---
+++ src/InterfazGrafica/Main.java
@@ -0,0 +1,10 @@
+
+package InterfazGrafica;
+
+import InterfazGrafica.iu.Principal;
+
+public class Main {
+    public static void main(String[] args) {
+        Principal ventana = new Principal("Hola mundo AWT");
+    }
+}
